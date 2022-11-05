bb27fad9ef844570d3e72945da00eb1d79e7fdaf
Julio Marques <juliomarquesjr@yahoo.com.br>
2022-11-05 08:21:19 -0300

    Finalização da tela de SignIn

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,11 +1,25 @@
-import { Center, Text } from "native-base";
-import React from "react";
+import { Center, Text, Icon } from "native-base";
+import { Fontisto } from "@expo/vector-icons";
+
+import Logo from "../assets/logo.svg";
+import { Button } from "../components/Button";
 
 export default function SiginIn() {
   return (
-    <Center flex={1} bgColor="gray.900">
+    <Center flex={1} bgColor="gray.900" p={7}>
       <Text color="white" fontSize={24}>
-        SignIn
+        <Logo width={212} height={40} />
+      </Text>
+      <Button
+        type="SECONDARY"
+        title="Entrar com o Google"
+        leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+        mt={12}
+      />
+
+      <Text color="white" textAlign="center" mt={4}>
+        Não utilizamos nenhuma informação além {'\n'} do seu e-mail para criação de sua
+        conta.
       </Text>
     </Center>
   );

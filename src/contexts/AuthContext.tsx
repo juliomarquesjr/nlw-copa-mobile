791ee9d1f5958512334b718062e0bc56672e34aa
Julio Marques <juliomarquesjr@yahoo.com.br>
2022-11-12 06:41:48 -0300

    Autenticação com o Google

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,9 @@
-import { createContext } from "react";
+import { createContext, ReactNode, useState, useEffect } from "react";
+import * as Google from "expo-auth-session/providers/google";
+import * as AuthSession from "expo-auth-session";
+import * as WebBrowser from "expo-web-browser";
+
+WebBrowser.maybeCompleteAuthSession();
 
 interface UserProps {
   name: string;
@@ -6,29 +11,56 @@ interface UserProps {
 }
 
 interface AuthProviderProps {
-  children: React.ReactNode;
+  children: ReactNode;
 }
 
 export interface AuthContextDataProps {
   user: UserProps;
+  isUserLoading: boolean;
   signIn: () => Promise<void>;
 }
 
 export const AuthContext = createContext({} as AuthContextDataProps);
 
 export function AuthContextProvider({ children }: AuthProviderProps) {
+  const [isUserLoading, setIsUserLoading] = useState(false);
+  const [user, setUser] = useState<UserProps>({} as UserProps);
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId:
+      "241402851087-rjga02jijsr867ktjn513ags2ue2k257.apps.googleusercontent.com",
+    redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
+    scopes: ["profile", "email"],
+  });
+
   async function signIn() {
-    console.log("Vamos logar");
+    try {
+      setIsUserLoading(true);
+      await promptAsync();
+    } catch (error) {
+      console.error(error);
+      throw error;
+    } finally {
+      setIsUserLoading(false);
+    }
   }
+
+  async function signInWithGoogle(acess_token: string) {
+    console.log("TOKEN DE CONFIRMAÇÃO : ===>", acess_token);
+  }
+
+  useEffect(() => {
+    if (response?.type === "success" && response.authentication?.accessToken) {
+      signInWithGoogle(response.authentication.accessToken);
+    }
+  }, [response]);
 
   return (
     <AuthContext.Provider
       value={{
         signIn,
-        user: {
-          name: "Julio Marques",
-          avatarUrl: "https://github.com/juliomarquesjr.png",
-        },
+        isUserLoading,
+        user,
       }}
     >
       {children}
